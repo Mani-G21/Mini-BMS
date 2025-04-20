a7cf3cce7f68124f5586b66c9ef2c775e72d4f8e
Ghindwani Manish <ghindwani.manish.st@gmail.com>
2025-04-20 18:38:47 +0530

    Chore: - Added $keyType = 'string' in order to make default type for id as string. - Removed the password column in User model i.e User.php and in create_users_table.

--- app/Core/Entities/User.php
+++ app/Core/Entities/User.php
@@ -3,15 +3,17 @@
 namespace App\Core\Entities;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Support\Str;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
+    protected $keyType = 'string';
     /**
      * The attributes that are mass assignable.
      *
@@ -20,7 +22,6 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
     ];
 
     /**
@@ -29,7 +30,6 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $hidden = [
-        'password',
         'remember_token',
     ];
 
@@ -42,7 +42,15 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
         ];
+    }
+
+    protected static function booted()
+    {
+        static::creating(function($user){
+            if(empty($user->id)){
+                $user->id = Str::uuid();
+            }
+        });
     }
 }
