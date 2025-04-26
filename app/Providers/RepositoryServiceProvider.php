eb263162a0d218b4d46d9976cbb95a5f2b28cb9e
Ghindwani Manish <ghindwani.manish.st@gmail.com>
2025-04-27 01:41:48 +0530

    Chore: - Binded the CityRepositoryInterface and MovieRepositoryInterface with their respective repositories.

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -4,8 +4,12 @@ namespace App\Providers;
 
 use App\Core\Interfaces\AuthRepositoryInterface;
 use App\Core\Interfaces\UserRepositoryInterface;
+use App\Domain\Movie\Repositories\CityRepositoryInterface;
+use App\Domain\Movie\Repositories\MovieRepositoryInterface;
 use App\Infrastructure\Auth\Repositories\AuthRepository;
 use App\Infrastructure\Auth\Repositories\UserRepository;
+use App\Infrastructure\Movie\Repositories\CityRepository;
+use App\Infrastructure\Movie\Repositories\MovieRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -17,6 +21,8 @@ class RepositoryServiceProvider extends ServiceProvider
     {
         $this->app->bind(AuthRepositoryInterface::class, AuthRepository::class);
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(CityRepositoryInterface::class, CityRepository::class);
+        $this->app->bind(MovieRepositoryInterface::class, MovieRepository::class);
     }
 
     /**
